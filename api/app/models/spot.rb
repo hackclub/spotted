f9d4d364136f8e47d3e02252b337d90318e87e37
Sam Poder <sam.r.poder@gmail.com>
2024-04-20 01:34:43 -0700

    add a basic validation

--- api/app/models/spot.rb
+++ api/app/models/spot.rb
@@ -4,6 +4,7 @@ class Spot < ApplicationRecord
   belongs_to :spotter, class_name: "User"
   belongs_to :spotted, class_name: "User"
   before_validate :calculate_points
+  validates :validate, on: :create
   
   has_one_attached :image
   
@@ -11,6 +12,12 @@ class Spot < ApplicationRecord
     points = 500
     if Spot.where(spotted_id: spotter_id, spotter_id: spotted_id).where("created_at > ?", 2.hours.ago).any?
       points += 300
+    end
+  end
+  
+  def validate
+    unless Spot.where(spotted_id:, spotter_id:).where("created_at > ?", 1.hours.ago).any?
+      errors.add(:base, "You can only spot people once every hour.")
     end
   end
 end
