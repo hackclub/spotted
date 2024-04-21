93905c809d0f709fe3c979eb50426b774848bc3f
Sam Poder <sam.r.poder@gmail.com>
2024-04-20 19:20:07 -0700

    `spotted_by_in_past_hour`

--- api/app/models/member.rb
+++ api/app/models/member.rb
@@ -6,4 +6,8 @@ class Member < ApplicationRecord
   has_many :spots, inverse_of: :spotted
 
   validates :user, :team, presence: true
+  
+  def spotted_by_in_past_hour
+    spots.where("spots.created_at > ?", 1.hour.ago).pluck(:spotter_id)
+  end
 end
