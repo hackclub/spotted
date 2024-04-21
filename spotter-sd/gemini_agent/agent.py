875b3e4b3706f23781cd5dc44a0bb84ac6de8c96
Robert Goll <robertrover811@outlook.com>
2024-04-20 21:22:23 -0700

    requests

--- spotter-sd/gemini_agent/agent.py
+++ spotter-sd/gemini_agent/agent.py
@@ -1,44 +1,45 @@
+import base64
 from uagents import Agent, Context, Protocol
 import aiohttp
 import google.generativeai as genai
 import PIL
 from PIL import Image
-import subprocess
 import io
 
-squirrelgetter = Agent(
-    name="squirrelgetter",
-    port=8000,
-    seed="squirrelgetter seed",
-    endpoint=["http://127.0.0.1:8000/squirrelnum"]
-)
+squirrelgetter = Agent(name="squirrelgetter")
 
 async def generate_response(model, prompt):
     response = await model.generate_content(prompt)
     return response['text'] if response else None
 
 @squirrelgetter.on_event("startup")
-async def process_image(ctx: Context):
+async def process_image(ctx: Context, request):
     ctx.logger.info('squirrelgetter online')
 
-    # image_url = 'https://cloud-8vxv0s664-hack-club-bot.vercel.app/4squirrel5.jpeg'
-    genai.configure(api_key = 'AIzaSyAbWV1gwiLRxuFFF15SjRUI7IYtVcyfdwA')
+    request_body = await request.read()
 
-    # curl_command = f"curl -o image.jpg {image_url}"
-    # subprocess.run(curl_command, shell=True)
+    image_data = io.BytesIO(request_body)
 
-    img = PIL.Image.open('image.jpg')
+    img = PIL.Image.open(image_data)
 
     if img:
-        ctx.logger.info('image fetched successfully')
+        ctx.logger.info('Image fetched successfully')
+
+        buffered = io.BytesIO()
+        img.save(buffered, format="JPEG")
+        img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
         model = genai.GenerativeModel('models/gemini-pro-vision')
 
-        response = model.generate_content(["If there are no squirrels in the image, output 0 and nothing else, no text. If there are squirrels in the image, please count the number of squirrels in this image. You may reason to yourself, but please put the final answer in curly braces and include just the number, nothing else.", img], stream=True)
+        response = model.generate_content(["If there are no squirrels in the image, output 0 and nothing else, no text. If there are squirrels in the image, please count the number of squirrels in this image. You may reason to yourself, but please put the final answer in curly braces and include just the number, nothing else.", 
+                                           
+                                           {
+                                               "mimeType": "image/jpeg",
+                                                  "data": img_base64
+                                           }], stream=True)
         response.resolve()
         ctx.logger.info(response.text)
         print(response.text)
 
 if __name__ == "__main__":
     squirrelgetter.run()
-
